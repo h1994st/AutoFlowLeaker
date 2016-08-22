339a336947a905a516dd8ff8ba219cf566bb89f0
h1994st <h1994st@gmail.com>
2016-08-22 16:07:30 +0800

    Evaluation: measure read/write/delete time (in China)

---
+++ eva_rwd_time.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-08-20 14:47:17
+# @Author  : Tom Hu (h1994st@gmail.com)
+# @Link    : http://h1994st.com
+# @Version : 1.0
+
+import json
+import time
+
+from Email import Email
+from Ghost import Ghost
+from Github import Github
+from Yinxiang import Yinxiang
+
+
+def test_email(data):
+    e = Email()
+    print 'Email'
+
+    # Write
+    print 'Write'
+    begin = time.time()
+    e.send_email(subject='from py %s hehehehe' % time.time(),
+                 text_body=data)
+    end = time.time()
+    write = end - begin
+
+    # Read: -1
+    # Delete: -1
+
+    return (-1, write, -1)
+
+
+def test_ghost(data):
+    g = Ghost()
+    print 'Ghost'
+
+    # Write
+    print 'Write'
+    begin = time.time()
+    post = g.create_post('from py %s' % time.time(), data)
+    end = time.time()
+    write = end - begin
+
+    # Read
+    print 'Read'
+    begin = time.time()
+    g.get_post(post['id'])
+    end = time.time()
+    read = end - begin
+
+    # Delete
+    print 'Delete'
+    begin = time.time()
+    g.delete_post(post['id'])
+    end = time.time()
+    delete = end - begin
+
+    return (read, write, delete)
+
+
+def test_github(data):
+    g = Github()
+    print 'Github'
+
+    # Write
+    print 'Write'
+    begin = time.time()
+    issue = g.create_issue('from py %s' % time.time(), data)
+    end = time.time()
+    write = end - begin
+
+    # Read
+    print 'Read'
+    begin = time.time()
+    g.get_issue(issue.number)
+    end = time.time()
+    read = end - begin
+
+    # Close
+    print 'Close'
+    begin = time.time()
+    g.close_issue(issue.number)
+    end = time.time()
+    delete = end - begin
+
+    return (read, write, delete)
+
+
+def test_yinxiang(data):
+    y = Yinxiang()
+    print 'Yinxiang'
+
+    # Write
+    print 'Write'
+    begin = time.time()
+    note = y.create_note('from py %s' % time.time(), data)
+    end = time.time()
+    write = end - begin
+
+    # Read
+    print 'Read'
+    begin = time.time()
+    y.get_note(note.guid)
+    end = time.time()
+    read = end - begin
+
+    # Delete
+    print 'Delete'
+    begin = time.time()
+    y.delete_note(note)
+    end = time.time()
+    delete = end - begin
+
+    return (read, write, delete)
+
+
+def main():
+    # emails = []
+    ghosts = []
+    githubs = []
+    yinxiangs = []
+
+    with open('temp.dat', 'r') as fp:
+        data = fp.read().strip()
+
+        for i in xrange(1):
+            # try:
+            #     temp = test_email(data)
+            # except KeyboardInterrupt:
+            #     break
+            # except:
+            #     temp = (-1, -1, -1)
+            # finally:
+            #     emails.append(temp)
+            #     print 'Email:', temp
+
+            try:
+                temp = test_ghost(data)
+            except KeyboardInterrupt:
+                break
+            except:
+                temp = (-1, -1, -1)
+            finally:
+                ghosts.append(temp)
+                print 'Ghost:', temp
+
+            try:
+                temp = test_github(data)
+            except KeyboardInterrupt:
+                break
+            except:
+                temp = (-1, -1, -1)
+            finally:
+                githubs.append(temp)
+                print 'Github:', temp
+
+            try:
+                temp = test_yinxiang(data)
+            except KeyboardInterrupt:
+                break
+            except:
+                temp = (-1, -1, -1)
+            finally:
+                yinxiangs.append(temp)
+                print 'Yinxiang:', temp
+
+    # Save
+    # with open('emails.log', 'w') as fp:
+    #     json.dump(emails, fp)
+
+    with open('ghosts.log', 'w') as fp:
+        json.dump(ghosts, fp)
+
+    with open('githubs.log', 'w') as fp:
+        json.dump(githubs, fp)
+
+    with open('yinxiangs.log', 'w') as fp:
+        json.dump(yinxiangs, fp)
+
+
+if __name__ == '__main__':
+    main()
