a08dccc09cdcdf3886da03405c1bac473b730889
h1994st <h1994st@gmail.com>
2016-08-26 01:01:50 +0800

    Update

--- cal_rwd_time.py
+++ cal_rwd_time.py
@@ -59,47 +59,81 @@ def cal_rwd_time(data):
 
 
 def mainland():
+    print '%10s  %8s, %8s, %8s' % ('Service', 'Read', 'Write', 'Delete')
     with open('data/bak/ghost_rwd_time.txt', 'r') as fp:
-        print 'Ghost...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Ghost', read_average, write_average, delete_average)
 
     with open('data/bak/github_rwc_time.txt', 'r') as fp:
-        print 'Github...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Github', read_average, write_average, delete_average)
 
     with open('data/bak/yinxiang_rwd_time.txt', 'r') as fp:
-        print 'Yinxiang...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Yinxiang', read_average, write_average, delete_average)
 
 
 def hongkong():
+    print '%10s  %8s, %8s, %8s' % ('Service', 'Read', 'Write', 'Delete')
     with open('data/ghost_rwd_time.txt', 'r') as fp:
-        print 'Ghost...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Ghost', read_average, write_average, delete_average)
 
     with open('data/github_rwc_time.txt', 'r') as fp:
-        print 'Github...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Github', read_average, write_average, delete_average)
 
     with open('data/yinxiang_rwd_time.txt', 'r') as fp:
-        print 'Yinxiang...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Yinxiang', read_average, write_average, delete_average)
 
     with open('data/wordpress_rwd_time.txt', 'r') as fp:
-        print 'Wordpress...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Wordpress', read_average, write_average, delete_average)
 
     with open('data/evernote_rwd_time.txt', 'r') as fp:
-        print 'Evernote...'
         data = json.load(fp)
-        pprint(cal_rwd_time(data))
+        result = cal_rwd_time(data)
+        read_average = result["read"] / result["read_count"]
+        write_average = result["write"] / result["write_count"]
+        delete_average = result["delete"] / result["delete_count"]
+        print '%10s  %.6f, %.6f, %.6f' % (
+            'Evernote', read_average, write_average, delete_average)
 
 if __name__ == '__main__':
     print 'China'
