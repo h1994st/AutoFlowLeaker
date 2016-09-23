4cc722e52da8501ad04e29e24bbe6ffbf1428fc0
h1994st <h1994st@gmail.com>
2016-09-23 17:22:42 +0800

    Update Github

--- Github.py
+++ Github.py
@@ -50,7 +50,6 @@ class Github(github.Github):
             'body': issue.body
         }
 
-    @timeout(TIMEOUT)
     def get_issues(self, readable=False):
         issues = self.repo.get_issues()
         if readable:
@@ -60,7 +59,7 @@ class Github(github.Github):
     # Write
     @timeout(TIMEOUT)
     def create_issue(self, title, body=github.GithubObject.NotSet,
-                     assignee=github.NamedUser.NamedUser):
+                     assignee=github.GithubObject.NotSet):
         return self.repo.create_issue(title, body=body, assignee=assignee)
 
     # Read
@@ -72,33 +71,100 @@ class Github(github.Github):
         return issue
 
     # Delete (impossible)
-    def delete_issue(self):
-        assert False, 'Impossible'
+    def delete_issue(self, number=None, issue=None):
+        self.close_issue(number=number, issue=issue)
 
     # Close
     @timeout(TIMEOUT)
-    def close_issue(self, number):
+    def close_issue(self, number=None, issue=None):
+        assert (number is None or
+                isinstance(number, int)), number
+        assert (issue is None or
+                isinstance(issue, github.Issue.Issue)), issue
+
+        if number is None and issue is None:
+            return
+
+        if issue is not None:
+            issue.edit(state='closed')
+            return
+
+        # issue is None, but number is not None
         self.get_issue(number).edit(state='closed')
 
     # Create a new repository
-    def create_repo(self, name='Test-Py'):
-        assert self.repo is None, self.repo
-
+    def create_repo(self, name=Config.Github('repo')):
         self._repo = self.user.create_repo(
             name, description='HEHEH', has_issues=True,
             has_wiki=False, has_downloads=False, auto_init=True)
 
-    # Delete a repository
-    def delete_repo(self):
-        assert self.repo is not None, self.repo
+        return self._repo
 
-        self.repo.delete()
-        self._repo = None
+    # Get a repository
+    def change_repo(self, name=None):
+        assert name is None or isinstance(name, (str, unicode)), name
+
+        if name is None:
+            # Change to default
+            name = Config.Github('repo')  # Default repository name
+
+        self._repo = self.user.get_repo(name)
+        return self._repo
+
+    # Delete a repository
+    def delete_repo(self, name=None):
+        assert name is None or isinstance(name, (str, unicode)), name
+
+        if name is None or name == self.repo.name:
+            # Current repo
+            repo = self.repo
+        else:
+            repo = self.user.get_repo(name)
+
+        if repo is None:
+            # no repo
+            print 'The repository you designate does not exist.'
+            return
+
+        # Delete
+        repo.delete()
+
+        # Change to default
+        self.change_repo()
 
     # Delete all issues
-    def delete_all_issues(self):
+    def delete_all_issues(self, name=None):
+        assert name is None or isinstance(name, (str, unicode)), name
+
         # Delete repo
-        self.delete_repo()
+        self.delete_repo(name=name)
 
         # Create new repo
-        self.create_repo()
+        self.create_repo(name=name)
+
+
+if __name__ == '__main__':
+    g = Github()
+
+    print g.repo
+
+    # Change repository
+    print g.change_repo('Yinxiang')
+
+    # Create repository
+    repository = g.create_repo(name='Test')
+
+    # Write
+    issue = g.create_issue('title', body='body')
+    print issue
+
+    # Read
+    print g.get_issue(issue.number)
+
+    # Delete (Close)
+    g.delete_issue(issue=issue)
+
+    # Delete repository
+    g.delete_repo()
+
+    print g.repo
