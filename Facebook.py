a1bf6dc870edd972c1249ea92b5bf4391fc070e0
h1994st <h1994st@gmail.com>
2016-11-24 15:16:30 +0800

    Add Facebook module

--- Facebook.py
+++ Facebook.py
@@ -5,8 +5,14 @@
 # @Link    : http://h1994st.com
 # @Version : 1.0
 
+import warnings
+from pprint import pprint
+
 import facebook
-import requests
+
+import Config
+
+warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 
 class Facebook(object):
@@ -15,4 +21,92 @@ class Facebook(object):
     '''
 
     def __init__(self):
-        pass
+        self._facebook_graph = facebook.GraphAPI(Config.Facebook('token'))
+
+    @property
+    def posts(self):
+        return self.get_posts()
+
+    def create_post(self, message):
+        assert len(message) <= 63206, \
+            'message cnnot be longer than 63206 characters'
+        try:
+            res = self._facebook_graph.put_object(
+                'me', 'feed', message=message)
+        except facebook.GraphAPIError as e:
+            print 'Something went wrong:', e.type, e.message
+            raise e
+        else:
+            return res['id']  # id of new post
+
+    def get_post(self, id):
+        try:
+            res = self._facebook_graph.get_object(id)
+        except facebook.GraphAPIError as e:
+            print 'Something went wrong:', e.type, e.message
+            raise e
+        else:
+            return res
+
+    def get_posts(self):
+        try:
+            posts = self._facebook_graph.get_connections('me', 'posts')
+        except facebook.GraphAPIError as e:
+            print 'Something went wrong:', e.type, e.message
+            raise e
+        else:
+            return posts['data']  # list of posts
+
+    def delete_post(self, id):
+        try:
+            self._facebook_graph.delete_object(id)
+        except facebook.GraphAPIError as e:
+            print 'Something went wrong:', e.type, e.message
+            raise e
+
+
+def test_fb_message_max():
+    fb = Facebook()
+
+    message = '1' * 63206
+
+    # Read all
+    pprint(fb.posts)
+
+    try:
+        # Write
+        post_id = fb.create_post(message)
+    except Exception as e:
+        print e
+    else:
+        # Read all
+        pprint(fb.posts)
+
+        # Delete
+        fb.delete_post(post_id)
+
+        # Read all
+        pprint(fb.posts)
+
+
+def main():
+    fb = Facebook()
+
+    # Read all
+    pprint(fb.posts)
+
+    # Write
+    post_id = fb.create_post('from python auto flow')
+
+    # Read all
+    pprint(fb.posts)
+
+    # Delete
+    fb.delete_post(post_id)
+
+    # Read all
+    pprint(fb.posts)
+
+
+if __name__ == '__main__':
+    test_fb_message_max()
