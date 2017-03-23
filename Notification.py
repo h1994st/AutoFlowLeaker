b8db67dd50a0af0bbdbd88c8c60ba14a3342d3b5
h1994st <h1994st@gmail.com>
2017-03-24 01:05:09 +0800

    Update configurations and Notification module

--- Notification.py
+++ Notification.py
@@ -11,12 +11,31 @@ import Config
 
 pb = Pushbullet(Config.Pushbullet('api_key'))
 
+DISABLE = (Config.Pushbullet('disable') == 'True')
+
+
+class VoidDevice(object):
+    def __init__(self):
+        super(VoidDevice, self).__init__()
+
+    def push_note(self, title, body):
+        print 'Push note:'
+        print '  title:', title
+        print '  body:', body
+
+
+void_device = VoidDevice()
+
 
 def get_devices():
+    if DISABLE:
+        return [void_device]
     return pb.devices
 
 
 def get_device(name):
+    if DISABLE:
+        return void_device
     return pb.get_device(name)
 
 
