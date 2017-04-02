889b424bf217eee84dd08df714385321823f3eb3
h1994st <h1994st@gmail.com>
2017-04-02 21:44:00 +0800

    Update

--- eva_file_time.py
+++ eva_file_time.py
@@ -5,16 +5,39 @@
 # @Link    : http://h1994st.com
 # @Version : 1.0
 
+import bitstring
+
 from auto_flow_leaker.ec_coder import ECCoder
 from auto_flow_leaker.comb_coder import Message
 from auto_flow_leaker.comb_coder import CombCoder
 
 M, R, I, K = 4, 2, 10, 3
 
-
 ec_coder = ECCoder(M, R)
+comb_coder = CombCoder(I, K)
+capacity = comb_coder.capacity  # in bits
 
 with open('data/eva_time_data_3.in', 'r') as fp:
     chunks = ec_coder.encode(fp.read())
-    print chunks
-    print [len(chunk) for chunk in chunks]
+
+total_chunk_len = len(chunks[0]) * len(chunk)  # in bytes
+all_chunk_data = b''.join(chunks)
+
+all_chunk_bitstring = bitstring.BitStream(bytearray(all_chunk_data))
+
+messages = []
+total_chunk_bits = len(all_chunk_bitstring)
+chunk_bit_offset = 0
+msg_size = capacity  # in bits
+
+while chunk_bit_offset < total_chunk_bits:
+    begin = chunk_bit_offset
+    end = chunk_bit_offset + msg_size
+    message = Message(
+        M, R,
+        total_chunk_bits, chunk_bit_offset,
+        data=all_chunk_bitstring[begin:end].tobytes())
+
+    messages.append(message)
+
+print messages
