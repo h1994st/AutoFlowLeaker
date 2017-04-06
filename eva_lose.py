747347c22bf354f72c3c48db913302ffe9df5f0b
h1994st <h1994st@gmail.com>
2017-04-06 22:18:52 +0800

    Update

--- eva_lose.py
+++ eva_lose.py
@@ -72,27 +72,50 @@ while num_loss > 0:
 
     num_loss -= 1
 
+# Recovered bitstring, including broken part (set as 0, if broken)
 recovered_bitstring = bitstring.BitStream(total_chunk_bits)
 for index in arrangement_indexs:
     arrangement = arrangements[index]
     message_integer = comb_coder.decode(*arrangement)
+
+    # [begin, end)
     begin = index * capacity
     end = min(begin + capacity, total_chunk_bits)
 
     recovered_bitstring[begin:end] = bitstring.BitStream(
         'uint:%d=%d' % (end - begin, message_integer))
 
+# Check broken indexs
+broken_chunk_indexs = set()
+for i in xrange(num_messages):
+    if i in arrangement_indexs:
+        continue
+
+    # [broken_begin, broken_end)
+    broken_begin = i * capacity
+    broken_end = min(broken_begin + capacity, total_chunk_bits)
+
+    broken_chunk_indexs.add(broken_begin / size_of_chunk)
+    broken_chunk_indexs.add(broken_end / size_of_chunk)
+
+print 'Broken chunks indexs:'
+print broken_chunk_indexs
+
 recovered_chunk_data = recovered_bitstring.bytes
 recovered_chunks = []
 for i in xrange(num_chunks):
+    # Skip broken chunks
+    if i in broken_chunk_indexs:
+        continue
+
     begin = i * size_of_chunk
     end = (i + 1) * size_of_chunk
+    recovered_chunks.append(recovered_chunk_data[begin:end])
 
-    if begin / 50 in arrangement_indexs and (end - 1) / 50 in arrangement_indexs:
-        recovered_chunks.append(recovered_chunk_data[begin:end])
+print 'Number of recovered chunks:', len(recovered_chunks)
+print 'Recovered chunk size:', len(recovered_chunks[0])
 
-print len(recovered_chunks)
-print len(recovered_chunks[0])
+
 recovered_data = ec_coder.decode(recovered_chunks)
 print recovered_data
 print recovered_data == DATA
